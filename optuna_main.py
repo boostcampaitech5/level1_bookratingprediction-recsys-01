5f7e2a0ae3cba424f0c144a0650e539635933194
juhyein <hyeinju304@gmail.com>
2023-04-17 17:50:49 +0000

    feat: add optuna_main

---
+++ optuna_main.py
@@ -0,0 +1,203 @@
+import time
+import argparse
+import wandb
+import os
+import dotenv
+import config
+import pandas as pd
+import numpy as np
+import optuna
+import matplotlib.pyplot as plt
+from optuna import Trial
+from optuna.samplers import TPESampler
+from optuna.integration.wandb import WeightsAndBiasesCallback
+from optuna.visualization import plot_param_importances, plot_optimization_history
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.model_selection import train_test_split, KFold
+from src.utils import Logger, Setting, models_load, get_timestamp
+from catboost import CatBoostRegressor, Pool
+from src.data import catboost_Data
+
+import warnings
+
+def rmse(real: list, predict: list) -> float:
+    '''
+    [description]
+    RMSE를 계산하는 함수입니다.
+
+    [arguments]
+    real : 실제 값입니다.
+    predict : 예측 값입니다.
+
+    [return]
+    RMSE를 반환합니다.
+    '''
+    pred = np.array(predict)
+    return np.sqrt(np.mean((real-pred) ** 2))
+
+def optuna_main(trial:Trial):
+    params = {
+        'iterations':trial.suggest_int("iterations", 500, 4000),
+        'learning_rate' : trial.suggest_float('learning_rate',0.001, 0.1),
+        'reg_lambda': trial.suggest_float('reg_lambda',1e-5,100),
+        'subsample': trial.suggest_float('subsample',0,1),
+        'random_strength': trial.suggest_float('random_strength',10,50),
+        'depth': trial.suggest_int('depth',1, 15),
+        'min_data_in_leaf': trial.suggest_int('min_data_in_leaf',1,30),
+        'leaf_estimation_iterations': trial.suggest_int('leaf_estimation_iterations',1,15),
+        'bagging_temperature' :trial.suggest_float('bagging_temperature', 0.01, 100.00),
+        }
+        
+    ######################## Load Dataset
+    train, test = catboost_Data(args)
+
+    X_train, y_train = train.drop(['user_id', 'isbn', 'rating','book_author'], axis=1), train['rating']
+    X_test, y_test = test.drop(['user_id', 'isbn', 'rating','book_author'], axis=1), test['rating']
+    
+    cat_list = [x for x in X_train.columns.tolist()]
+        
+    # modelling - Catboost
+
+    '''
+    [description]
+    CatBoost 모델을 사용하여 예측값을 출력하는 부분입니다.
+
+    [arguments]
+    objective: loss-function
+    n_estimators: iterations
+    eta: learning-rate
+    task_type : for using GPU
+    verbose: 실험 결과 출력 간격.
+    cat_features: 사용할 범주형 feature 
+    random_seed: seed
+    use-best-model : If this parameter is set, the number of trees that are saved in the resulting model is defined.
+    output-borders-file
+    '''
+
+    tr_X, val_X, tr_y, val_y = train_test_split(X_train,
+                                                    y_train,
+                                                    test_size = 0.2,
+                                                    random_state= args.seed,
+                                                    shuffle=True
+                                                        )
+    if args.model == 'catboost':
+        model = CatBoostRegressor(**params,
+                              task_type = "GPU",
+                              cat_features = cat_list,
+                              random_seed= args.seed,
+                              bootstrap_type='Poisson',
+                              verbose = 100)
+    model.fit(tr_X,tr_y, use_best_model = True, eval_set = (val_X, val_y))
+
+    val_pred = model.predict(val_X)
+    val_pred = val_pred.tolist()
+    val_RMSE = rmse(val_y, val_pred)
+    return(val_RMSE)
+
+    
+
+if __name__ == "__main__":
+
+
+    ######################## BASIC ENVIRONMENT SETUP
+    parser = argparse.ArgumentParser(description='parser')
+    arg = parser.add_argument
+
+    ############### WANDB OPTION
+    arg('--project', type=str, default='Tree-based models')
+    arg('--entity', type=str, default='recsys01')
+
+    ############### BASIC OPTION
+    arg('--data_path', type=str, default='../data/', help='Data path를 설정할 수 있습니다.')
+    arg('--data_shuffle', type=bool, default=True, help='데이터 셔플 여부를 조정할 수 있습니다.')
+    arg('--test_size', type=float, default=0.2, help='Train/Valid split 비율을 조정할 수 있습니다.')
+    arg('--seed', type=int, default=42, help='seed 값을 조정할 수 있습니다.')
+    arg('--model', type=str, default='catboost', choices=['catboost','lgbm'],
+                                help='학습 및 예측할 모델을 선택할 수 있습니다.')
+    arg('--device', type=str, default='cuda', choices=['cuda', 'cpu'], help='학습에 사용할 Device를 조정할 수 있습니다.')
+    arg('--name', type=str, default=f'work-{get_timestamp()}')
+    arg('--saved_model_path', type=str, default='./saved_models', help='Saved Model path를 설정할 수 있습니다.')
+    arg('--use_best_model', type=bool, default=True, help='검증 성능이 가장 좋은 모델 사용여부를 설정할 수 있습니다.')
+    
+
+    ############### TRAINING OPTION
+    arg('--loss_fn', type=str, default='RMSE', choices=['MSE', 'RMSE'], help='손실 함수를 변경할 수 있습니다.')
+    arg('--process_cat', type=str, default='basic', choices=['basic', 'high'], help='books 데이터의 카테고리를 선택할 수 있습니다.')
+    arg('--process_age', type=str, default='global_mean', choices=['global_mean', 'zero_cat','stratified', 'knn', 'rand_norm'], help='데이터의 결측치를 처리할 방법을 선택할 수 있습니다.')
+    arg('--process_loc', type=str, nargs='+', default=['city', 'state', 'country'], choices=['none', 'city', 'state', 'country'], help='usesr의 location을 구분할 기준을 선택할 수 있습니다. none을 선택하면 location은 drop됩니다.')
+    
+    ############### OPTUNA OPTION
+    arg('--n_trials', type = int, default = 20, help = "Search 횟수를 설정할 수 있습니다.")
+
+    args = parser.parse_args()
+
+    ######################## Setting for saving
+    setting = Setting(args)
+
+    log_path = setting.get_log_path(args)
+    setting.make_dir(log_path)
+    filename = setting.get_submit_filename(args)
+
+
+    ####################### Wandb logging
+    wandb_kwargs = {"project": "Tree-based models",
+                    "entity" : 'recsys01',
+                    'name' : args.name,
+                    'tags':args.model,
+                    "reinit": True}
+    wandbc = WeightsAndBiasesCallback(metric_name="RMSE", wandb_kwargs=wandb_kwargs)
+    
+    optuna_cbrm = optuna.create_study(direction='minimize', sampler = TPESampler())
+    optuna_cbrm.optimize(optuna_main, n_trials = args.n_trials , callbacks =[wandbc])    
+
+    train, test = catboost_Data(args)
+    X_train, y_train = train.drop(['user_id', 'isbn', 'rating','book_author'], axis=1), train['rating']
+    X_test, y_test = test.drop(['user_id', 'isbn', 'rating','book_author'], axis=1), test['rating']
+
+    
+
+    f = "best_{}".format
+    for param_name, param_value in optuna_cbrm.best_trial.params.items():
+        wandb.run.summary[f(param_name)] = param_value
+        wandb.run.summary["best val_RMSE"] = optuna_cbrm.best_trial.value
+    
+    
+    wandb.log({"param_importance_chart" : plot_param_importances(optuna_cbrm) ,
+               "param_optimization_history" : plot_optimization_history(optuna_cbrm)})
+
+    ####################### Final train&prediction using best model
+    print(f"=============best trial value : {optuna_cbrm.best_trial.value}=============")
+    print(f"=============best trial parameter : {optuna_cbrm.best_trial.params}=============")
+    
+
+     
+    ####################### Train& predict using best params
+    cat_list = [x for x in X_train.columns.tolist()]
+    best_params = optuna_cbrm.best_trial.params
+    if args.model == 'catboost':
+        best_model = CatBoostRegressor(**best_params,
+                                   task_type = "GPU",
+                                   cat_features = cat_list,
+                                   random_seed= args.seed,
+                                   bootstrap_type='Poisson',
+                                   verbose = 100)
+        
+
+    best_model.fit(X_train, y_train)
+    predicts = best_model.predict(X_test)
+
+    os.makedirs(args.saved_model_path, exist_ok=True)
+    saved_model_path = f"{args.saved_model_path}/{setting.save_time}_{args.model}_model.cbm"
+    best_model.save_model(saved_model_path)
+    wandb.save(saved_model_path)
+    wandb.save(log_path+f'{args.model}_params.json')
+    
+    ######################## SAVE PREDICT
+    print(f'--------------- PREDICTING {args.model} ---------------')
+    submission = pd.read_csv(args.data_path + 'sample_submission.csv')
+    submission['rating'] = predicts
+    
+    print(f'--------------- SAVE {filename} ---------------')
+    submission.to_csv(filename, index=False)
+    wandb.save(filename)
+    
