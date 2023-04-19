6334df163b57938ad835233b140d968cfb4b4f47
alstjrdlzz <tjalstjr95@gmail.com>
2023-04-19 04:10:47 +0000

    refactor: change some typo after local test

--- src/train/trainer.py
+++ src/train/trainer.py
@@ -6,12 +6,12 @@ import torch.nn as nn
 import numpy as np
 from torch.utils.data import Subset, DataLoader
 from sklearn.model_selection import KFold
-from src.utils import get_sampler
+from src.utils import get_sampler, models_load, loss_fn_load, optimizer_load
 
 
 def train(args, dataloader, logger, setting, need_log=True, fold=""):
     # init model, loss_fn, optimizer
-    model = models_load(args, data)
+    model = models_load(args, dataloader)
     loss_fn = loss_fn_load(args)
     optimizer = optimizer_load(args, model)
 
@@ -62,7 +62,7 @@ def train(args, dataloader, logger, setting, need_log=True, fold=""):
         else:
             best_loss = valid_loss
             patient_check = 0     
-    
+    print(f"score: {minimum_loss:4f}")
     return model, minimum_loss
 
 def cv_train(args, dataloader, logger, setting):
@@ -81,6 +81,7 @@ def cv_train(args, dataloader, logger, setting):
         model, minimum_loss = train(args, dataloader, logger, setting, need_log=False, fold=str(fold))
         model_list.append(model)
         cv_score += minimum_loss/5
+    print(f"cv_score: {cv_score:4f}")
     return model_list, cv_score
 
 def valid(args, model, dataloader, loss_fn):
@@ -104,7 +105,6 @@ def valid(args, model, dataloader, loss_fn):
     valid_loss = total_loss/batch
     return valid_loss
 
-
 def test(args, model, dataloader, setting, fold=""):
     predicts = list()
     model.load_state_dict(torch.load(f'./saved_models/{setting.save_time}_{args.model}_model{fold}.pt'))
@@ -126,17 +126,17 @@ def test(args, model, dataloader, setting, fold=""):
 
 def oof_test(args, model_list, dataloader, setting):
     predicts_list = []
-    for fold in range(len(model_list)):
+    for fold, model in enumerate(model_list):
         predicts_fold = np.array(test(args, model, dataloader, setting, str(fold)))
         predicts_list.append(predicts_fold)
 
-    predicts = np.sum(predicts_list, axis=0) / k
+    predicts = np.sum(predicts_list, axis=0) / len(model_list)
     return list(predicts)
 
-def infer(args, model, dataloader, setting):
+def infer(args, model, dataloader, setting, fold=""):
     predicts = list()
     if args.use_best_model == True:
-        model.load_state_dict(torch.load(f'./saved_models/{setting.save_time}_{args.model}_model.pt'))
+        model.load_state_dict(torch.load(f'./saved_models/{setting.save_time}_{args.model}_model{fold}.pt'))
     else:
         pass
     model.eval()
@@ -153,10 +153,8 @@ def infer(args, model, dataloader, setting):
         y_hat = model(x)
         predicts.extend(y_hat.tolist())
     
-
     user_id = dataloader['X_valid']['user_id'].map(dataloader['idx2user']).reset_index(drop=True)
     isbn = dataloader['X_valid']['isbn'].map(dataloader['idx2isbn']).reset_index(drop=True)
     rating = dataloader['y_valid'].reset_index(drop=True)
     pred = pd.Series(predicts, name='pred')
-
     return pd.concat([user_id, isbn, rating, pred], axis=1)
