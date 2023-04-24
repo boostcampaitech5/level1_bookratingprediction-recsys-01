b1efaf6d2fd0cbd3530ad2d43b74f27dfb465b6d
juhyein <hyeinju304@gmail.com>
2023-04-24 13:53:44 +0000

    refactor: prediction functions

--- optuna_main.py
+++ optuna_main.py
@@ -13,6 +13,7 @@ from optuna.integration.wandb import WeightsAndBiasesCallback
 from optuna.visualization import plot_param_importances, plot_optimization_history
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from src.utils import Setting, get_timestamp, rmse
+from src.TreeBased import grid_search, Valid, OOF, Test
 from catboost import CatBoostRegressor
 from lightgbm import LGBMRegressor 
 from src.data import TreeBase_data
@@ -38,7 +39,7 @@ def CB_optuna(trial:Trial):
             'objective': 'regression',
             'verbosity': -1,
             'metric': 'rmse', 
-            'num_iterations' : 2500, #trial.suggest_int('num_iterations', 1000, 2000),
+            'num_iterations' : 10, #trial.suggest_int('num_iterations', 1000, 2000),
             'num_leaves' : 9 , #trial.suggest_int('num_leaves', 3, 9),
             'max_depth': trial.suggest_int('max_depth',4, 9),
             'learning_rate' : trial.suggest_float('learning_rate',0.01, 0.1),
@@ -51,10 +52,10 @@ def CB_optuna(trial:Trial):
             }
 
     ######################## Load Dataset
-    X_train, y_train, X_test, y_test, cat_list = TreeBase_data(args) 
-
-    tr_X, val_X, tr_y, val_y = train_test_split(X_train,
-                                                    y_train,
+    data = TreeBase_data(args) #data[0], data[1], data[2], data[3], data[-1]
+    cat_list = data[-1]
+    tr_X, val_X, tr_y, val_y = train_test_split(data[0],
+                                                    data[1],
                                                     test_size = 0.2,
                                                     random_state= args.seed,
                                                     shuffle=True
@@ -106,10 +107,8 @@ if __name__ == "__main__":
     
 
     ############### TRAINING OPTION
+    arg('--n_fold', type=int, default= 5,  help= '교차검증 방식을 변경할 수 있습니다.')
     arg('--cv', type=str, default= 'Hold_out', choices=['Hold_out', 'K_fold'], help='교차검증 방식을 변경할 수 있습니다.')
-    arg('--loss_fn', type=str, default='RMSE', choices=['MSE', 'RMSE'], help='손실 함수를 변경할 수 있습니다.')
-    arg('--optimizer', type=str, default='ADAM', choices=['SGD', 'ADAM'], help='최적화 함수를 변경할 수 있습니다.')
-    arg('--weight_decay', type=float, default=1e-6, help='Adam optimizer에서 정규화에 사용하는 값을 조정할 수 있습니다.')
     arg('--process_cat', type=str, default='basic', choices=['basic', 'high'], help='books 데이터의 카테고리를 선택할 수 있습니다.')
     arg('--process_age', type=str, default='global_mean', choices=['global_mean', 'zero_cat','stratified', 'knn', 'rand_norm'], help='데이터의 결측치를 처리할 방법을 선택할 수 있습니다.')
     arg('--process_feat', type=str, nargs='+', default=[], choices=['user_id', 'age', 'isbn', 'category', 'publisher', 'language', 'book_author','year_of_publication', 'location_city', 'location_state', 'location_country'], help='Tree model에서 drop할 feature를 고를 수 있습니다.')
@@ -140,10 +139,11 @@ if __name__ == "__main__":
     optuna_cbrm.optimize(CB_optuna, n_trials = args.n_trials , callbacks =[wandbc])    
 
     ####################### data load for prediction
-    X_train, y_train, X_test, y_test, cat_list = TreeBase_data(args)
+    data = TreeBase_data(args) 
 
-    tr_X, val_X, tr_y, val_y = train_test_split(X_train,
-                                                y_train,
+    cat_list = data[-1]
+    tr_X, val_X, tr_y, val_y = train_test_split(data[0],
+                                                data[1],
                                                 test_size = args.test_size,
                                                 random_state= args.seed,
                                                 shuffle=True)
@@ -175,44 +175,17 @@ if __name__ == "__main__":
                                        random_seed= args.seed,
                                        bootstrap_type='MVS',
                                        verbose = 100)
-        # valid prediction
         best_model.fit(tr_X, tr_y)
-        val_pred= best_model.predict(val_X)
-
-        best_model.fit(X_train, y_train)
-        predicts = best_model.predict(X_test)
-
-        ## OOF Prediction
-        oof_result = []
-        for tr, val in cv.split(X_train, y_train):
-            tr_x , tr_y = X_train.iloc[tr], y_train.iloc[tr]
-            
-            best_model.fit(tr_x, tr_y)
-            pred_oof = best_model.predict(X_test).tolist()
-            oof_result.append(pred_oof)
-
-        oof_pred = list(np.sum(oof_result, axis= 0) / 5)
-
-
+        
     elif args.model == 'lgbm':
         best_model = LGBMRegressor(**best_params,cat_feature = cat_list)
         best_model.fit(tr_X, tr_y, eval_set = (val_X, val_y),eval_metric = 'rmse', verbose = 500, early_stopping_rounds=10)
-        
-        val_pred = best_model.predict(val_X)
+    
+    
 
-        best_model.fit(X_train, y_train)
-        predicts = best_model.predict(X_test)
-
-        ## OOF prediction
-        oof_result = []
-        for tr, val in cv.split(X_train, y_train):
-            tr_x , tr_y = X_train.iloc[tr], y_train.iloc[tr]
-            
-            best_model.fit(tr_x, tr_y)
-            pred_oof = best_model.predict(X_test).tolist()
-            oof_result.append(pred_oof)
-
-        oof_pred = list(np.sum(oof_result, axis= 0) / 5)
+    valid_pred = Valid(data, best_model, args)
+    oof_pred = OOF(data, best_model, args)
+    predicts = Test(data, best_model)
     
 
     os.makedirs(args.saved_model_path, exist_ok=True)
@@ -240,7 +213,7 @@ if __name__ == "__main__":
     
 
     val_result = pd.read_csv(args.data_path + 'sample_validation.csv')
-    val_result['pred'] = val_pred
+    val_result['pred'] = valid_pred
     val_result.to_csv(filename.replace('.csv', '_valid.csv'), index=False)
     
 
