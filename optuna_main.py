67dc88dc9efe1ec08f2a7d49091ada285835538b
juhyein <hyeinju304@gmail.com>
2023-04-24 13:26:17 +0000

    refactor: TreeBase_data

--- optuna_main.py
+++ optuna_main.py
@@ -7,40 +7,22 @@ import config
 import pandas as pd
 import numpy as np
 import optuna
-import lightgbm as lgb
-import matplotlib.pyplot as plt
 from optuna import Trial
 from optuna.samplers import TPESampler
 from optuna.integration.wandb import WeightsAndBiasesCallback
 from optuna.visualization import plot_param_importances, plot_optimization_history
 from sklearn.model_selection import train_test_split, StratifiedKFold
-from src.utils import Logger, Setting, models_load, get_timestamp
-from catboost import CatBoostRegressor, Pool
+from src.utils import Setting, get_timestamp, rmse
+from catboost import CatBoostRegressor
 from lightgbm import LGBMRegressor 
-from src.data import catboost_Data
+from src.data import TreeBase_data
 import warnings
-
 warnings.filterwarnings("ignore")
-
-def rmse(real: list, predict: list) -> float:
-    '''
-    [description]
-    RMSE를 계산하는 함수입니다.
-
-    [arguments]
-    real : 실제 값입니다.
-    predict : 예측 값입니다.
-
-    [return]
-    RMSE를 반환합니다.
-    '''
-    pred = np.array(predict)
-    return np.sqrt(np.mean((real-pred) ** 2))
 
 def CB_optuna(trial:Trial):
     if args.model == 'catboost':
         params = {
-            'iterations':trial.suggest_int("iterations", 1500, 3500),
+            'iterations':trial.suggest_int("iterations", 5, 10),
             'learning_rate' : trial.suggest_float('learning_rate',0.001, 0.1),
             'reg_lambda': trial.suggest_float('reg_lambda',50,150),
             'subsample': trial.suggest_float('subsample',0.5,1),
@@ -69,17 +51,7 @@ def CB_optuna(trial:Trial):
             }
 
     ######################## Load Dataset
-    train, test = catboost_Data(args)
-
-    X_train, y_train = train.drop(['rating', 'language',  'category'], axis=1), train['rating']
-    X_test, y_test = test.drop(['rating', 'language',  'category'], axis=1), test['rating']
-    
-    cat_list = [x for x in X_train.columns.tolist()]
-
-    if args.model == 'lgbm':
-        X_test = X_test.astype('category')
-        X_train[cat_list] = X_train[cat_list].astype('category')
-
+    X_train, y_train, X_test, y_test, cat_list = TreeBase_data(args) 
 
     tr_X, val_X, tr_y, val_y = train_test_split(X_train,
                                                     y_train,
@@ -140,7 +112,7 @@ if __name__ == "__main__":
     arg('--weight_decay', type=float, default=1e-6, help='Adam optimizer에서 정규화에 사용하는 값을 조정할 수 있습니다.')
     arg('--process_cat', type=str, default='basic', choices=['basic', 'high'], help='books 데이터의 카테고리를 선택할 수 있습니다.')
     arg('--process_age', type=str, default='global_mean', choices=['global_mean', 'zero_cat','stratified', 'knn', 'rand_norm'], help='데이터의 결측치를 처리할 방법을 선택할 수 있습니다.')
-    arg('--process_loc', type=str, nargs='+', default=['city', 'state', 'country'], choices=['none', 'city', 'state', 'country'], help='usesr의 location을 구분할 기준을 선택할 수 있습니다. none을 선택하면 location은 drop됩니다.')
+    arg('--process_feat', type=str, nargs='+', default=[], choices=['user_id', 'age', 'isbn', 'category', 'publisher', 'language', 'book_author','year_of_publication', 'location_city', 'location_state', 'location_country'], help='Tree model에서 drop할 feature를 고를 수 있습니다.')
     
     ############### OPTUNA OPTION
     arg('--n_trials', type = int, default = 20, help = "Search 횟수를 설정할 수 있습니다.")
@@ -168,28 +140,13 @@ if __name__ == "__main__":
     optuna_cbrm.optimize(CB_optuna, n_trials = args.n_trials , callbacks =[wandbc])    
 
     ####################### data load for prediction
-    train, test = catboost_Data(args)
-    # user_id, isbn 제외하고 나머지 사용하지 않는 변수 미리 drop
-    train = train.drop(['language',  'category'], axis = 1)
-    test = test.drop(['language',  'category'], axis = 1)
-
-
-    X_train, y_train = train, train['rating']
-    X_test, y_test = test.drop(['rating'], axis=1), test['rating']
-
+    X_train, y_train, X_test, y_test, cat_list = TreeBase_data(args)
 
     tr_X, val_X, tr_y, val_y = train_test_split(X_train,
                                                 y_train,
                                                 test_size = args.test_size,
                                                 random_state= args.seed,
                                                 shuffle=True)
-    
-    tr_X = tr_X.drop(['rating'], axis = 1)
-    val_result = val_X[['user_id','isbn','rating']]
-    val_X = val_X.drop(['rating'], axis = 1)
-    
-    X_train = X_train.drop(['rating'], axis = 1)
-    cat_list = X_train.columns.tolist()
 
     f = "best_{}".format
     for param_name, param_value in optuna_cbrm.best_trial.params.items():
@@ -206,7 +163,6 @@ if __name__ == "__main__":
     print(f"=============best trial parameter : {optuna_cbrm.best_trial.params}=============")
     
 
-
     ####################### Train& predict using best params
     
     best_params = optuna_cbrm.best_trial.params
@@ -221,7 +177,7 @@ if __name__ == "__main__":
                                        verbose = 100)
         # valid prediction
         best_model.fit(tr_X, tr_y)
-        val_result['pred'] = best_model.predict(val_X)
+        val_pred= best_model.predict(val_X)
 
         best_model.fit(X_train, y_train)
         predicts = best_model.predict(X_test)
@@ -238,20 +194,11 @@ if __name__ == "__main__":
         oof_pred = list(np.sum(oof_result, axis= 0) / 5)
 
 
-
-
-
     elif args.model == 'lgbm':
-        tr_X = tr_X.astype('category')
-        val_X = val_X.astype('category')
-        best_model = LGBMRegressor(**best_params,
-                                   cat_feature = cat_list)
+        best_model = LGBMRegressor(**best_params,cat_feature = cat_list)
         best_model.fit(tr_X, tr_y, eval_set = (val_X, val_y),eval_metric = 'rmse', verbose = 500, early_stopping_rounds=10)
-
         
-        val_result['pred'] = best_model.predict(val_X)
-        X_train = X_train.astype('category')
-        X_test = X_test.astype('category')
+        val_pred = best_model.predict(val_X)
 
         best_model.fit(X_train, y_train)
         predicts = best_model.predict(X_test)
@@ -279,34 +226,25 @@ if __name__ == "__main__":
     
     wandb.save(log_path+f'{args.model}_params.json')
     
-    ######################## SAVE PREDICT
-    print(f'--------------- PREDICTING SUBMISSIONS  ---------------')
+    ######################## SAVE RESULTS
+
+    print(f'--------------- SAVING RESULTS  ---------------')
     submission = pd.read_csv(args.data_path + 'sample_submission.csv')
     submission['rating'] = predicts
-
-    '''submission.to_csv(filename, index=False)
-    wandb.save(filename)'''
-
+    submission.to_csv(filename, index=False)
+    
+    
     oof_submission = pd.read_csv(args.data_path + 'sample_submission.csv')
     oof_submission['rating'] = oof_pred
-    
-    print(f'--------------- SAVEING RESULTS ---------------')
-    
-    val_result.to_csv(filename.replace('.csv', '_valid.csv'), index=False)
-    wandb.save(filename.replace('.csv', '_valid.csv'))
-
     oof_submission.to_csv(filename.replace('.csv', '_oof.csv'), index=False)
+    
+
+    val_result = pd.read_csv(args.data_path + 'sample_validation.csv')
+    val_result['pred'] = val_pred
+    val_result.to_csv(filename.replace('.csv', '_valid.csv'), index=False)
+    
+
+    ###################### SAVE FILES TO WANDB
     wandb.save(filename.replace('.csv', '_oof.csv'))
-
-
-    filename = setting.get_submit_filename(args)
-    oof_name = filename.replace('.csv', '_oof.csv')
-    val_name = filename.replace('.csv', '_valid.csv')
-
-    submission.to_csv(filename, index=False)
-    oof_submission.to_csv(oof_name, index=False)
-    val_result.to_csv(val_name, index=False)
-
-    wandb.save(filename)
-    wandb.save(oof_name)
-    wandb.save(val_name)
+    wandb.save(filename.replace('.csv', '_oof.csv'))
+    wandb.save(filename.replace('.csv', '_valid.csv'))
